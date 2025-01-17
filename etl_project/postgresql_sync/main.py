b5feb8c6c1615149da6cd3249470a65d2456e62f
saeed <sa.shirazi@jabama..com>
2025-01-17 12:47:04 +0330

    update output

--- etl_project/postgresql_sync/main.py
+++ etl_project/postgresql_sync/main.py
@@ -23,6 +23,19 @@ def main():
     for arg, value in vars(args).items():
         print(f"{arg}: {value}")
 
+#output
+# Parsed Arguments:
+# db_type: postgresql
+# db_ip: localhost
+# db_port: 5432
+# db_name: pyspark_training
+# db_username: pyspark_user
+# db_password: password
+# table_name: etl_transformed_data
+# path: /home/saeed/PycharmProjects/pyspark_training_codes/etl_project/postgresql_sync/pyspark_training/data
+# strategy: append
+# unique_id: id
+
 
 if __name__ == "__main__":
     main()
